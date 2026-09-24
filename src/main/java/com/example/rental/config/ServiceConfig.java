c209399c29d542678a23e13fd43b9e33fdbc8198
Aruzhan <s23068986@narxoz.kz>
2026-09-24 21:34:33 +0500

    Update ServiceConfig.java

--- src/main/java/com/example/rental/config/ServiceConfig.java
+++ src/main/java/com/example/rental/config/ServiceConfig.java
@@ -1,4 +1,18 @@
 package com.example.rental.config;
 
-public class ServiceConfig {
+import com.example.rental.domain.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configurationpublic class ServiceConfig {
+
+    @Bean
+    public RentalPolicy rentalPolicy() {
+        return new RentalPolicy(List.of(
+            new TransitionRule(),
+            new StopFactorRule()
+        ));
+    }
 }
