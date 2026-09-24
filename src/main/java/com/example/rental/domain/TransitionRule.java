ad9ba123f604caa5d79293b0bde6eb9e06c4d1e6
gulnazkaiyrzhankyzy08e3 <gulnaz.kaiyrzhankyzy@narxoz.kz>
2026-09-24 22:29:24 +0600

    Update TransitionRule.java

--- src/main/java/com/example/rental/domain/TransitionRule.java
+++ src/main/java/com/example/rental/domain/TransitionRule.java
@@ -1,4 +1,10 @@
 package com.example.rental.domain;
 
-public class TransitionRule {
+public class StopFactorRule implements RentalRule {
+    @Override
+    public void check(RentalStatus from, RentalStatus to) {
+        if (from instanceof RentalStatus.Booked && to instanceof RentalStatus.Returned) {
+            throw new IllegalStateException("Стоп-фактор: Көйлек әлі берілген жоқ!");
+        }
+    }
 }
