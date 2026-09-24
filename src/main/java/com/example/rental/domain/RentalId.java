a947190086b93e3690c0be0235643421b43dbcdf
Aruzhan <s23068986@narxoz.kz>
2026-09-24 21:32:16 +0500

    Update RentalId.java

--- src/main/java/com/example/rental/domain/RentalId.java
+++ src/main/java/com/example/rental/domain/RentalId.java
@@ -3,7 +3,7 @@ package com.example.rental.domain;
 public record RentalId(String value) {
     public RentalId {
         if (value == null || value.isBlank()) {
-            throw new IllegalArgumentException("Rental ID cannot be null or blank");
+            throw new IllegalArgumentException("ID бос болмауы керек!");
         }
     }
 }
