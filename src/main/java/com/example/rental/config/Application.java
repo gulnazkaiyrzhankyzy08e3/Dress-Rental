2a0814030f9661e26f32872d49aeae95e9be777c
gulnazkaiyrzhankyzy08e3 <gulnaz.kaiyrzhankyzy@narxoz.kz>
2026-09-24 22:33:38 +0600

    Update Application.java

--- src/main/java/com/example/rental/config/Application.java
+++ src/main/java/com/example/rental/config/Application.java
@@ -1,4 +1,11 @@
 package com.example.rental.config;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
